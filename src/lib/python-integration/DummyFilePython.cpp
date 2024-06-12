e5a2e1d6b688e8d6dc384eaba1328ae333cf9b30
Bastien Levasseur <bastien.levasseur3@orange.fr>
2024-06-12 10:40:53 +0200

    Added very basic cmake configuration and dummy python integration file

---
+++ src/lib/python-integration/DummyFilePython.cpp
@@ -0,0 +1,14 @@
+#include "DummyFilePython.hpp"
+#include <iostream>
+
+namespace MALT{
+
+
+void dummyPythonFunction(){
+    std::cout << "Dummy Python Integration Function !" << std::endl;
+}
+
+
+
+}
+
---
+++ src/lib/python-integration/DummyFilePython.hpp
@@ -0,0 +1,11 @@
+#ifndef DummyFilePython_hpp
+#define DummyFilePython_hpp
+
+
+namespace MALT{
+
+void dummyPythonFunction();
+
+}
+
+#endif //DummyFilePython_hpp
